3526ce27929f191e373e2b5ffa3ec641f83b8a1d
Isaac Frias <isaacfrias123@gmail.com>
2025-08-12 20:50:31 -0700

    Add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import EvolucionTelefonicaPreset from './theme-preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: EvolucionTelefonicaPreset,
+        options: {
+          darkModeSelector: false || 'none'
+        }
+      },
+      ripple: true,
+
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, ToolbarModule, ButtonModule, PanelMenuModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('evolucion-telefonica-admin');
+  menu = [
+    { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
+    {
+      label: 'Products', icon: 'pi pi-mobile',
+      items: [
+        { label: 'Phones', icon: 'pi pi-phone', routerLink: '/phones' },
+        { label: 'Plans', icon: 'pi pi-list', routerLink: '/plans' }
+      ]
+    },
+    { label: 'Support', icon: 'pi pi-question-circle', routerLink: '/support' }
+  ];
 }
